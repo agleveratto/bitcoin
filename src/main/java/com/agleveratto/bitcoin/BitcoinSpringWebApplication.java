fc0cfe89c8a35f6d51c389d42b13666d11ed306c
agleveratto <agleveratto@gmail.com>
2021-09-16 11:24:45 -0300

    conf: initial configuration

---
+++ src/main/java/com/agleveratto/bitcoin/BitcoinSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.agleveratto.bitcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinSpringWebApplication.class, args);
+    }
+
+}
